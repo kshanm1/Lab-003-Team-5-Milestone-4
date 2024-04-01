e2e6674af677580488afdc43fcf4ba17d5d185a3
Kirisanth Shanmugalingam <kshanm@uwo.ca>
2024-04-01 15:39:00 -0400

    Sorting Code V2

--- SortingTestCode/SortingTestCode.ino
+++ SortingTestCode/SortingTestCode.ino
@@ -1,10 +1,10 @@
 // 
-// MSE 2202 TCS34725 colour sensor example
+// MSE 2202 Milestone 4 Sorting Code
 // 
 //  Language: Arduino (C++)
 //  Target:   ESP32-S3
-//  Author:   Michael Naish
-//  Date:     2024 03 05 
+//  Author:   Lab 03 Team 5
+//  Date:     2024 04 01 
 //
 
 #define PRINT_COLOUR                                  // uncomment to turn on output of colour sensor data
@@ -13,6 +13,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <Servo.h>
 #include "Adafruit_TCS34725.h"
 
 // Function declarations
@@ -26,31 +27,22 @@ const int cSDA               = 47;                    // GPIO pin for I2C data
 const int cSCL               = 48;                    // GPIO pin for I2C clock
 const int cTCSLED            = 14;                    // GPIO pin for LED on TCS34725
 const int cLEDSwitch         = 46;                    // DIP switch S1-2 controls LED on TCS32725    
+const int cDCMotor           = 25;                    // GPIO pin for DC motor
+const int cServo             = 26;                    // GPIO pin for servo motor
 
 // Variables
 boolean heartbeatState       = true;                  // state of heartbeat LED
 unsigned long lastHeartbeat  = 0;                     // time of last heartbeat state change
 unsigned long curMillis      = 0;                     // current time, in milliseconds
 unsigned long prevMillis     = 0;                     // start time for delay cycle, in milliseconds
+Servo servo;                                           // servo object
 
-// Declare SK6812 SMART LED object
-//   Argument 1 = Number of LEDs (pixels) in use
-//   Argument 2 = ESP32 pin number 
-//   Argument 3 = Pixel type flags, add together as needed:
-//     NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//     NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//     NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//     NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//     NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel SmartLEDs(cSmartLEDCount, cSmartLED, NEO_RGB + NEO_KHZ800);
 
-// Smart LED brightness for heartbeat
 unsigned char LEDBrightnessIndex = 0; 
 unsigned char LEDBrightnessLevels[] = {0, 0, 0, 5, 15, 30, 45, 60, 75, 90, 105, 120, 135, 
                                        150, 135, 120, 105, 90, 75, 60, 45, 30, 15, 5, 0};
 
-// TCS34725 colour sensor with 2.4 ms integration time and gain of 4
-// see https://github.com/adafruit/Adafruit_TCS34725/blob/master/Adafruit_TCS34725.h for all possible values
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X);
 bool tcsFlag = 0;                                     // TCS34725 flag: 1 = connected; 0 = not found
 
@@ -67,6 +59,11 @@ void setup() {
   Wire.setPins(cSDA, cSCL);                           // set I2C pins for TCS34725
   pinMode(cTCSLED, OUTPUT);                           // configure GPIO to control LED on TCS34725
   pinMode(cLEDSwitch, INPUT_PULLUP);                  // configure GPIO to set state of TCS34725 LED 
+  pinMode(cDCMotor, OUTPUT);                          // configure GPIO to control DC motor
+  analogWrite(cDCMotor, 128);                         // set DC motor to half speed
+
+  servo.attach(cServo);                               // attach servo motor to its pin
+  servo.write(90);                                    // set initial position to 90 degrees
 
   // Connect to TCS34725 colour sensor
   if (tcs.begin()) {
@@ -85,9 +82,12 @@ void loop() {
   digitalWrite(cTCSLED, !digitalRead(cLEDSwitch));    // turn on onboard LED if switch state is low (on position)
   if (tcsFlag) {                                      // if colour sensor initialized
     tcs.getRawData(&r, &g, &b, &c);                   // get raw RGBC values
-#ifdef PRINT_COLOUR            
-      Serial.printf("R: %d, G: %d, B: %d, C %d\n", r, g, b, c);
-#endif
+    Serial.printf("R: %d, G: %d, B: %d, C %d\n", r, g, b, c);
+    if (g > r && g > b) {                             // if green is the dominant colour
+      servo.write(180);                               // turn servo to the right
+    } else {
+      servo.write(0);                                 // turn servo to the left
+    }
   }
   doHeartbeat();                                      // update heartbeat LED
 }
